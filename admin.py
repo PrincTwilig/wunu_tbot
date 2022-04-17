9ad0ace2a711184bbef2e31b316f3d64cb59a0fc
PrincTwilig <93429979+PrincTwilig@users.noreply.github.com>
2022-04-17 23:06:50 +0300

    comit

--- admin.py
+++ admin.py
@@ -1,12 +1,16 @@
 import telebot
 bot = telebot.TeleBot('2030518741:AAGIODEOhmrpWEhoZ9z8u4roDjhbLHrnyV8')
 
+user_private_talk = ''
+admin_private_talk = ''
+
 def adminka(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     itembtn1 = telebot.types.KeyboardButton('1. Скачати файл користувачів')
     itembtn2 = telebot.types.KeyboardButton('2. Відправити повідомлення всім користувачам')
-    itembtn3 = telebot.types.KeyboardButton('Назад')
-    markup.add(itembtn1, itembtn2, itembtn3)
+    itembtn3 = telebot.types.KeyboardButton('3. Говорити з користувачем по id')
+    itembtn4 = telebot.types.KeyboardButton('Назад')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
     msg = bot.send_message(message.chat.id, 'Виберіть дію:', reply_markup=markup)
     bot.register_next_step_handler(msg, admin_menu)
 
@@ -17,11 +21,40 @@ def admin_menu(message):
         elif message.text == '2. Відправити повідомлення всім користувачам':
             msg = bot.reply_to(message, 'Введіть повідомлення:')
             bot.register_next_step_handler(msg, admin_send_message)
+        elif message.text == '3. Говорити з користувачем по id':
+            msg = bot.reply_to(message, 'Введіть id користувача, щоб закінчити приватний чат, нажміть "Назад":')
+            bot.register_next_step_handler(msg, admin_talk_with_user)
         elif message.text == 'Назад':
             exit(message)
         else:
             bot.send_message(message.chat.id, 'Невірна команда')
             adminka(message)
+    except Exception as e:
+        bot.reply_to(message, 'Помилка: ' + str(e))
+
+def admin_talk_with_user(message):
+    try:
+        global admin_private_talk, user_private_talk
+        admin_id = str(message.chat.id)
+        admin_private_talk = admin_id
+        user_private_talk = message.text
+        msg = bot.reply_to(message, 'Введіть повідомлення:')
+        bot.register_next_step_handler(msg, admin_talk_with_user_send)
+    except Exception as e:
+        bot.reply_to(message, 'Помилка: ' + str(e))
+
+def admin_talk_with_user_send(message):
+    try:
+        global admin_private_talk, user_private_talk
+        if message.text == 'Назад':
+            admin_private_talk = None
+            user_private_talk = None
+            adminka(message)
+            return 0
+        print(str(message.chat.username) + ": Тільки що відправив повідомлення користувачу " + user_private_talk + ": " + message.text)
+        bot.send_message(user_private_talk, message.text)
+        msg = bot.send_message(message.chat.id, 'Введіть повідомлення:')
+        bot.register_next_step_handler(msg, admin_talk_with_user_send)
     except Exception as e:
         bot.reply_to(message, 'Помилка: ' + str(e))
 
@@ -56,3 +89,11 @@ def admin_download(message):
 def exit(message):
     # вихід з адмінки
     bot.send_message(message.chat.id, 'Вихід з адмінки')
+
+def admin_id_back():
+    global admin_private_talk
+    return admin_private_talk
+
+def user_id_back():
+    global user_private_talk
+    return user_private_talk
